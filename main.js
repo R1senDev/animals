aa2a39833621f27489b517833a2b6bd872013167
R1senDev <skip.py@mail.ru>
2022-08-18 21:54:36 +0400

    Update

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-let cursor = {
-	aim: {
-		animated: false,
-		frames: [new Image(20, 20)]
-	},
-};
-
-cursor.aim.frames[0].src = 'cursors/aim/1.png';
-
 let gamepadAPI = {
 	controller: {},
 	connected: false,
@@ -87,7 +78,7 @@ window.addEventListener('gamepaddisconnected', gamepadAPI.disconnect);
 
 let kbKeysPressed = new Map();
 
-let map = [['deepWater', 'water', 'shoreWater', 'sand', 'sand', 'grass']];
+let map = [[]];
 
 class Animal {
 	constructor(x, y, type) {
@@ -156,21 +147,129 @@ class Animal {
 	}
 }
 
+let pixelSize = 10;
+let mapSize = [40, 40];
+let generatorProperties = {
+	// This is not the actual size of the island, but a side
+	// setting of the generator, only indirectly affecting its
+	// size. Setting a value of 100 corresponds to a strict
+	// (minimumWaterPercent * 100)% of water and
+	// (100 - minimumWaterPercent * 100)% of land, but a
+	// value of 99 does not guarantee that the island
+	// consist of two or three cells (albeit with a small
+	// probability).
+	islandSize: 98,
+	minimumWaterPercent: 0.25,
+};
+
+function generateWorld(type) {
+	map = [];
+	switch (type) {
+		case 'classic':
+			for (let e = 0; e < mapSize[1]; e++) {
+				map.push([]);
+			}
+			for (let y = 0; y < mapSize[1]; y++) {
+				for (let x = 0; x < mapSize[0]; x++) {
+					map[x][y] = 'deepWater';
+				}
+			}
+
+			let rPos = [Math.floor(Math.random() * mapSize[0]), Math.floor(Math.random() * mapSize[1])];
+			map[rPos[0]][rPos[1]] = 'grass';
+			let rSpr = 0;
+			let spawnPos = 0;
+			while (rSpr < generatorProperties.islandSize) {
+				for (let y = 0; y < mapSize[1]; y++) {
+					for (let x = 0; x < mapSize[0]; x++) {
+						if (map[x][y] == 'grass') {
+							spawnPos = Math.floor(Math.random() * 4);
+							switch (spawnPos) {
+								case 0:
+									try {
+										map[x][y - 1] = 'grass';
+									} catch {}
+									break;
+								case 1:
+									try {
+										map[x + 1][y] = 'grass';
+									} catch {}
+									break;
+								case 2:
+									try {
+										map[x][y + 1] = 'grass';
+									} catch {}
+									break;
+								case 3:
+									try {
+										map[x - 1][y] = 'grass';
+									} catch {}
+									break;
+							}
+						}
+					}
+				}
+
+				// [water, land]
+				let tilesCount = [0, 0];
+				for (let y = 0; y < mapSize[1]; y++) {
+					for (let x = 0; x < mapSize[0]; x++) {
+						if (map[x][y] == 'grass') {
+							tilesCount[1] += 1;
+						} else {
+							tilesCount[0] += 1;
+						}
+					}
+				}
+				if (tilesCount[0] / tilesCount[1] <= generatorProperties.minimumWaterPercent) {
+					break;
+				}
+
+				rSpr = Math.random() * 101;
+			}
+
+			break;
+
+		case 'debug':
+			map = [
+				['deepWater', 'water', 'shoreWater', 'sand', 'grass', 'fire'],
+				['coal1', 'coal2', 'coal3'],
+			];
+			break;
+	}
+}
+
 function setup() {
 	createCanvas(window.innerWidth, window.innerHeight);
+	generateWorld('classic');
 }
+
+let fps = 0;
+let fps_ = 0;
+
+let ups = 0;
+let ups_ = 0;
 
 function tick() {
-
+	ups_ += 1;
 }
 
-let pixelSize = 50;
+setInterval(tick, 10);
+setInterval(function() {
+	ups = ups_;
+	ups_ = 0;
+
+	fps = fps_;
+	fps_ = 0;
+
+	console.log(`FPS: ${fps} | UPS: ${ups}`);
+}, 1000);
 
 function draw() {
 	noStroke();
+
 	for (let x = 0; x < map.length; x++) {
 		for (let y = 0; y < map[x].length; y++) {
-			console.log(`Reading map[${x}][${y}]`);
 			switch (map[x][y]) {
 				case 'sand':
 					fill(color('#fcdd76'));
@@ -187,8 +286,36 @@ function draw() {
 				case 'shoreWater':
 					fill(color('#5294ff'));
 					break;
+				case 'fire':
+					fill(color('#eb8d00'));
+					break;
+				case 'coal':
+					let rc = Math.floor(Math.random() * 3);
+					switch (rc) {
+						case 0:
+							fill(color('#434750'));
+							break;
+						case 1:
+							fill(color('#303136'));
+							break;
+						case 2:
+							fill(color('#293133'));
+							break;
+					}
+					break;
+				case 'coal1':
+					fill(color('#353942'));
+					break;
+				case 'coal2':
+					fill(color('#303136'));
+					break;
+				case 'coal3':
+					fill(color('#293133'));
+					break;
 			}
 			rect(y * pixelSize, x * pixelSize, pixelSize, pixelSize);
 		}
 	}
+
+	fps_ += 1;
 }
